66abde81b09d8e859f104e7bba886108f4994073
Eric MacAdie <Eric@MacAdie.net>
2014-10-28 02:34:56 -0500

    Working

--- someserver.groovy
+++ someserver.groovy
@@ -5,16 +5,20 @@ while ( true ) {
     server.accept {  socket ->
         println "processing new connection..."
         socket.withStreams { input, output ->
+            println "input is a ${input.class.name}"
             def reader = input.newReader()
+            println "reader is a ${reader.class.name}"
             def buffer = reader.readLine()
             println "server received: $buffer"
+            println "can reader still be read? ${reader.ready()}"
             now = new Date()
             output << "echo-response($now): " + buffer + "\n"
         }
         println "processing/thread complete."
     }
 }
-
+// input is a java.net.SocketInputStream
+// reader is a java.io.BufferedReader
 /*
 To use:
 s = new Socket("localhost", 4444);
