c2bec5339b7e8edcf90c684bbc9a585ebe929aaa
EMacAdie <EMacAdie9@gmail.com>
2015-04-12 23:07:15 -0500

    Updated build.gradle

--- src/main/groovy/info/shelfunit/mail/MailRunner.groovy
+++ src/main/groovy/info/shelfunit/mail/MailRunner.groovy
@@ -5,6 +5,7 @@ class MailRunner {
     static main( args ) {
         // def fGS = new FirstGroovyServer()
         // fGS.doStuff( Integer.parseInt( args[ 0 ] ) )
+        println "in MailRunner"
         SMTPServer smtp = new SMTPServer()
         smtp.doStuff( 25 )
     }
