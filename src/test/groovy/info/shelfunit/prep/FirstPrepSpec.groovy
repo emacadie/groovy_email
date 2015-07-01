10a1efc0bb5cc4938311061476a20bfa22047a13
EMacAdie <EMacAdie@gmail.com>
2015-07-01 01:02:13 -0500

    I added some tests to work with databases

--- src/test/groovy/info/shelfunit/prep/FirstPrepSpec.groovy
+++ src/test/groovy/info/shelfunit/prep/FirstPrepSpec.groovy
@@ -10,11 +10,15 @@ import spock.lang.Requires
 import org.junit.Rule
 import org.junit.rules.TestName
 
+import groovy.sql.Sql
+
 @Stepwise
 class FirstPrepSpec extends Specification {
     
     @Shared
     static prepDatabase = false
+    
+    static sql
     
     @Rule 
     TestName name = new TestName()
@@ -31,6 +35,16 @@ class FirstPrepSpec extends Specification {
         if ( System.getProperty( "database.prep" ) == 'true' ) {
             println "database.prep is true"
             prepDatabase = true
+            /*
+            systemProperties[ 'dbname' ]        = 'gemail_test_db'
+            systemProperties[ 'host_and_port' ] = 'localhost:5432'
+            systemProperties[ 'dbuser' ]        = 'gemail_test'
+            systemProperties[ 'dbpassword' ]    = 'dev-word-to-pass001'
+            */
+            def db = [url: "jdbc:postgresql://${System.properties[ 'host_and_port' ]}/${System.properties[ 'dbname' ]}",
+            user: System.properties[ 'dbuser' ], password: System.properties[ 'dbpassword' ], driver: 'org.postgresql.Driver']
+            sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+            
         } else {
             println "database.prep not set"
             prepDatabase = false
@@ -55,11 +69,29 @@ class FirstPrepSpec extends Specification {
             2 == 2
     }
     
-    @Requires({ System.properties[ 'database.prep' ] == 'true' })
+    // in the closure for Requires, you can use "properties" instead of "System.properties"
+    @Requires({ properties[ 'database.prep' ] == 'true' })
     def "run if database prep is true"() {
         println "prepDatabase: ${prepDatabase}"
         expect:
             4 == 4
+    }
+    
+    @Requires({ properties[ 'database.prep' ] == 'true' })
+    def "create user table"() {
+        
+        sql.execute '''
+            CREATE TABLE email_user (
+                userid serial primary key NOT NULL,
+                username character varying(64) not null unique,
+                password_hash character varying(150) not null,
+                password_algo character varying(32) not null,
+                first_name character varying(30) not null,
+                last_name character varying(30) not null,
+                version bigint NOT NULL
+            )'''
+         expect:
+            5 == 5
     }
 }
 
