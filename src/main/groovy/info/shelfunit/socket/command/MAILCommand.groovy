5503a2d9331249cb157d991236aef23880e4cb07
EMacAdie <EMacAdie@gmail.com>
2015-07-06 00:41:58 -0500

    Working on tests

--- src/main/groovy/info/shelfunit/socket/command/MAILCommand.groovy
+++ src/main/groovy/info/shelfunit/socket/command/MAILCommand.groovy
@@ -14,97 +14,38 @@ class MAILCommand {
     static regex = '''^(MAIL FROM):<[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’ # WTF?
 *+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}>$(?x)'''
 /*
+ regex = '''^(MAIL FROM):<([\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’ # WTF?
+*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+([a-zA-Z]{2,6}))>$(?x)'''
 regexB = '''^(MAIL FROM):<[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}>$'''
 */
     static pattern = ~regex
     
     def resultMap = [:]
-    def process( theMessage, prevCommandList ) {
-        def resultString
-        // if () { }
-        
-    }
     
     def process( theMessage, prevCommandList, bufferMap ) {
         def resultString
         resultMap.clear()
-        bufferMap.reversePath = '' 
-        bufferMap.forwardPath = ''
-        bufferMap.mailData = ''
-        
-        if ( !theMessage.startsWith( 'MAIL FROM:' ) ) {
-            resultMap.resultString = "501 Command not in proper form"
-        } else if ( !prevCommandList.last.matches( 'EHLO|HELO|RSET' ) ) {
+        // bufferMap.reversePath = '' 
+        // bufferMap.forwardPath = ''
+        // bufferMap.mailData = ''
+        def regexResult = ( theMessage ==~ pattern )
+        if ( !prevCommandList.last().matches( 'EHLO|HELO|RSET' ) ) {
             resultMap.resultString = "503 Bad sequence of commands"
-        } else if ( !( theMessage ==~ pattern) ) {
+        } else if ( !theMessage.startsWith( 'MAIL FROM:' ) ) {
+            resultMap.resultString = "501 Command not in proper form"
+        } else if ( !regexResult ) {
+            resultMap.resultString = "501 Command not in proper form"
+        } else if ( !( theMessage ==~ pattern ) ) {
             resultMap.resultString = "501 Command not in proper form"
         } else {
             prevCommandList << 'MAIL'
-            resultMap.prevCommandList = prevCommandList
             resultMap.resultString = '250 OK'
-            def q = theMessage ==~ patterm
-            bufferMap.reversePath = q[0][1] + "@" + q[0][2]
+            def q = theMessage ==~ pattern
+            bufferMap.reversePath =  q[ 0 ][ 2 ]
             resultMap.bufferMap = bufferMap
         }
-        /*
-pattern = ~/^.*?groovy.*$/
-===> ^.*?groovy.*$
-pattern.class.name
-===> java.util.regex.Pattern
-input = 'i love me some groovy code'
-input ==~ pattern
-===> true
-groovy:000> input =~ pattern
-mPattern = ~/MAIL\sFROM:<\s[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})>$/
-                                                [_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})
-mP = ~/^MAIL FROM:<(.+)@(.+)>$/
+        resultMap.prevCommandList = prevCommandList
 
-~/^(MAIL FROM:)<(.+)@(.+)>$/
-~/^(MAIL FROM:)<(.+)@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})>$/
-
-~/^(MAIL FROM:)<[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})?$/
-
-~/^(MAIL FROM:)<[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})>$/
-q = mm =~ mP // get the matcher as q
-to view the matches
-q[0][1]
-I have no idea why you need a 2-dimensional array
-~/^(MAIL FROM:)<(.+)@(.+)>$(?x)/ (?x) for comments at the end
-
-EMAIL_PATTERN = ~/^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$/
-
-^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})
-
-EMAIL_PATTERN = ~/[a-z[._-][\\d]]*[@][a-z[.][\\d]]*[.][a-z[.][\\d]]* /
-EMAIL_PATTERN =~/.+@.+\\.[a-z]+/
-EMAIL_PATTERN =~ /^[A-Z0-9+_.-]+@[A-Z0-9.-]+$/
-['mkyong@yahoo.com', 'mkyong-100@yahoo.com', 'mkyong.100@yahoo.com',
-'mkyong111@mkyong.com', 'mkyong-100@mkyong.net', 'mkyong.100@mkyong.com.au',
-'mkyong@1.com', 'mkyong@gmail.com.com', 'mkyong+100@gmail.com', 'mkyong-100@yahoo-test.com'].each {
-    println "looking at ${it}"
-    print it ==~ EMAIL_PATTERN
-    println "; done with ${it}"
-} 
-compile 'commons-validator:commons-validator:1.4.1'
-        */
-        /*
-        def domain = theMessage.replaceFirst( 'EHLO |HELO ', '' )
-        log.info "Here is the domain: ${domain}"
-        if ( domain.length() > 255 ) {
-            resultMap.resultString = "501 Domain name length beyond 255 char limit per RFC 3696"
-            resultMap.prevCommandList = prevCommandList
-        } else if ( ( domain.length() <= 255 ) && ( theMessage.firstFour() == 'EHLO' ) ) {
-            prevCommandList.clear()
-            prevCommandList << 'EHLO'
-            resultMap.resultString = "250-Hello ${domain}\n250 HELP"
-		} else if ( ( domain.length() <= 255 ) && ( theMessage.firstFour() == 'HELO' ) ) {
-		    prevCommandList.clear()
-		    prevCommandList << 'HELO'
-		    resultMap.resultString = "250 Hello ${domain}"
-		}
-		resultMap.prevCommandList = prevCommandList
-		resultMap.bufferMap = bufferMap
-		*/
 		log.info "here is resultMap: ${resultMap.toString()}"
 		resultMap
     }
---
+++ src/test/groovy/info/shelfunit/socket/command/MAILCommandSpec.groovy
@@ -0,0 +1,139 @@
+package info.shelfunit.socket
+
+import spock.lang.Specification
+
+import org.junit.Rule
+import org.junit.rules.TestName
+
+import info.shelfunit.mail.MailRunner
+import info.shelfunit.socket.command.MAILCommand
+
+import org.xbill.DNS.Address
+
+import groovy.mock.interceptor.StubFor
+
+class MAILCommandSpec extends Specification {
+    
+    def crlf = "\r\n"
+
+    @Rule 
+    TestName name = new TestName()
+    
+    def setup() {
+        println "\n--- Starting test ${name.methodName}"
+    }          // run before every feature method
+    def cleanup() {}        // run after every feature method
+    def setupSpec() {
+        MailRunner.runMetaProgramming()
+    }     // run before the first feature method
+    
+    def cleanupSpec() {}   // run after the last feature method
+
+	def "test handling EHLO"() {
+	    def mailCommand = new MAILCommand()
+	    
+	    when:
+	        def resultMap = mailCommand.process( "MAIL FROM:<oneill@stargate.mil>", ['RCPT'], [:] )
+	        def mailResponse = resultMap.resultString + crlf 
+	    then:
+	        mailResponse == "503 Bad sequence of commands\r\n"
+	        resultMap.prevCommandList == [ "RCPT" ]
+	}
+	/*
+	def "test handling HELO"() {
+	    def ehloCommand = new EHLOCommand()
+	    
+	    def domain = "hot-groovy.com"
+	    when:
+	        def resultMap = ehloCommand.process( "HELO ${domain}", [], [:] )
+	        def ehloResponse = resultMap.resultString + crlf
+	    then:
+	        ehloResponse == "250 Hello ${domain}\r\n"
+	}
+	*/
+	/*
+	def "test handling domain more than 255 char"() {
+	    def ehloCommand = new EHLOCommand()
+	    
+	    def longString = ( "f" * 252 ) + '.com'
+	    when:
+	        def resultMap = ehloCommand.process( "EHLO ${longString}", [], [:] )
+	        def ehloResponse = resultMap.resultString + crlf 
+	    then:
+	        ehloResponse == "501 Domain name length beyond 255 char limit per RFC 3696\r\n"
+	}
+	*/
+	/*
+	def "test that command list and buffer map are cleared"() {
+	    def domain = "www.groovymail.org"
+	    def ehloCommand = new EHLOCommand()
+	    
+	    when:
+	        def prevCommandList = [ 'Get the hell off my ship', 'keep us under the radar, Wash' ]
+	        def bufferMap = [ name:'Jayne', location: 'bunk' ]
+	    then:
+	        prevCommandList.size() == 2
+	        bufferMap.size() == 2
+	    when:
+	        def resultMap = ehloCommand.process( "EHLO ${domain}", prevCommandList, bufferMap )
+	        def ehloResponse = resultMap.resultString + crlf 
+	        def newList = resultMap.prevCommandList
+	        def newMap = resultMap.bufferMap
+	    then:
+	        ehloResponse == "250-Hello ${domain}\n" +
+	        "250 HELP\r\n"
+	        newList.size() == 1
+	        newList[ 0 ] == "EHLO"
+	        newMap.size() == 0
+	}
+	*/
+	/*
+	def "test getting address"() {
+	    def serverName = "www.groovymail.org"
+	    def ehloCommand = new EHLOCommand()
+	    def inetAddress = Stub( InetAddress )
+	    def address = GroovyStub( Address )
+	    address.getByName(_) >> inetAddress
+	    inetAddress.hostAddress >> 'X.X.X.X'
+	    def result = ehloCommand.processDomain( "XYZ" )
+	    println "here is result: ${result}"
+	    expect:
+	        result == 'X.X.X.X'
+	        1 == 1
+	}
+	*/
+	/*
+	def "test without mocks or stubs"() {
+	    when:
+            def domain = "www.shelfunit.info"
+            def ehloCommand = new EHLOCommand()
+            def result = ehloCommand.processDomain( domain )
+            println "here is result: ${result}"
+	    then:
+	        result == '45.33.18.182'
+	}
+	*/
+	// this works even if not connected to internet
+	/*
+	def "test good domain with mocks or stubs"() {
+	    when:
+            def domain = "www.shelfunit.info"
+            def ehloCommand = new EHLOCommand()
+            def inetAddress = GroovyStub( InetAddress )
+            inetAddress.hostAddress >> '45.33.18.182'
+            def result = ''
+            def stub = new StubFor( Address )      
+            stub.demand.with {                  
+                getByName{ inetAddress }
+            }
+            stub.use {                          
+                result = ehloCommand.processDomain( domain )
+            }
+            println "here is result: ${result}"
+	    then:
+	        stub.expect.verify() 
+	        result == '45.33.18.182'
+	}
+	*/
+}
+
