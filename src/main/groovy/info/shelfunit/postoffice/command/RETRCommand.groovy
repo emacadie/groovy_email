b046b12db4da83e0683a7ebccafd29a68ea069b3
EMacAdie <EMacAdie@gmail.com>
2015-09-11 02:07:27 -0500

    Working on RETRCommand

---
+++ src/main/groovy/info/shelfunit/postoffice/command/RETRCommand.groovy
@@ -0,0 +1,64 @@
+package info.shelfunit.postoffice.command
+
+import groovy.sql.Sql
+import groovy.util.logging.Slf4j 
+
+import visibility.Hidden
+
+@Slf4j
+class RETRCommand {
+    
+    @Hidden def regex = "RETR\\s\\d+"
+    
+    @Hidden Sql sql
+    RETRCommand( def argSql ) {
+        log.info "Starting new RETRCommand"
+        this.sql = argSql
+    }
+    
+    def process( theMessage, prevCommandSet, bufferMap ) {
+        log.info "Starting LISTCommand.process"
+   
+        def resultString
+        def resultMap = [:]
+        resultMap.clear()
+        log.info "Here is bufferMap: ${bufferMap}"
+        log.info "Does bufferMap.hasSTATInfo() sez the lolcat ? let's find out: ${bufferMap.hasSTATInfo()}"
+        if ( !bufferMap.hasSTATInfo() ) {
+            bufferMap.getSTATInfo( sql )
+        }
+        log.info "Does bufferMap.hasSTATInfo() sez the lolcat ? let's find out: ${bufferMap.hasSTATInfo()}"
+        if ( bufferMap.state != 'TRANSACTION' ) {
+            resultMap.resultString = "-ERR Not in TRANSACTION state"
+        } else if ( !theMessage.matches( regex ) ) {
+            resultMap.resultString = "-ERR Command not in proper form"
+        } else if ( theMessage.matches( regex ) ) {
+            log.info "in the reg ex part"
+            def messageNum = Integer.parseInt( theMessage.allButFirstFour().trim() )
+            if ( messageNum > bufferMap.uuidList.size() ) {
+                resultMap.resultString = "-ERR no such message, only ${bufferMap.uuidList.size()} messages in maildrop"
+            } else {
+                def uuid = bufferMap.uuidList[ messageNum - 1 ].id
+                log.info "here is bufferMap.uuidList: ${bufferMap.uuidList}"
+                log.info "uuid is a ${uuid.getClass().name}"
+                def ans = sql.firstRow( "select length( text_body ),text_body from mail_store where id = ?", [ uuid ] )
+                if ( ans.isEmpty() ) {
+                    resultMap.resultString = "-ERR no such message, only ${bufferMap.uuidList.size()} messages in maildrop"
+                } else {
+                    def sBuff = new StringBuffer()
+                    sBuff << "+OK ${ans.length} octets\r\n"
+                    sBuff << ans.text_body
+                    sBuff << "\r\n"
+                    sBuff << "."
+                    resultMap.resultString = sBuff.toString() 
+                }
+            }
+        }
+        resultMap.bufferMap = bufferMap
+        resultMap.prevCommandSet = prevCommandSet
+
+		log.info "here is resultMap: ${resultMap.toString()}"
+		resultMap
+    }
+}
+
---
+++ src/test/groovy/info/shelfunit/postoffice/command/RETRCommandSpec.groovy
@@ -0,0 +1,196 @@
+package info.shelfunit.postoffice.command
+
+import java.sql.Timestamp
+
+// import spock.lang.Ignore
+import spock.lang.Specification
+import spock.lang.Stepwise
+// import spock.lang.Unroll
+
+import org.junit.Rule
+import org.junit.rules.TestName
+
+import info.shelfunit.mail.MetaProgrammer
+import info.shelfunit.mail.ConfigHolder
+
+import groovy.sql.Sql
+import groovy.util.logging.Slf4j 
+
+import org.apache.shiro.crypto.hash.Sha512Hash
+
+@Slf4j
+@Stepwise
+class RETRCommandSpec extends Specification {
+    
+    def crlf = "\r\n"
+    static domainList = [ 'shelfunit.info', 'groovy-is-groovy.org' ]
+    static sql
+    static iterations = 10000
+    static retrCommand
+    static somePassword = 'somePassword'
+    static theTimestamp
+    static gwRETR  = 'gwretr' // @shelfunit.info'
+    static jaRETR = 'jaretr' // @shelfunit.info'
+    static joRETR = 'joretr' // @shelfunit.info'
+    static uuidA = UUID.randomUUID()
+    static uuidB = UUID.randomUUID()
+    static uuidC = UUID.randomUUID()
+    static msgA = 'aq' * 10
+    static msgB = 'bq' * 11
+    static msgC = 'cq' * 12
+    
+    @Rule 
+    TestName name = new TestName()
+    
+    def setup() {
+        println "\n--- Starting test ${name.methodName}"
+    }          // run before every feature method
+    def cleanup() {}        // run after every feature method
+    
+    def setupSpec() {
+        MetaProgrammer.runMetaProgramming()
+        
+        ConfigHolder.instance.setConfObject( "src/test/resources/application.test.conf" )
+        def conf = ConfigHolder.instance.getConfObject()
+        log.info "conf is a ${conf.class.name}"
+        def db = [ url: "jdbc:postgresql://${conf.database.host_and_port}/${conf.database.dbname}",
+        user: conf.database.dbuser, password: conf.database.dbpassword, driver: conf.database.driver ]
+        log.info "db is a ${db.getClass().name}"
+        sql = Sql.newInstance( db.url, db.user, db.password, db.driver )
+        this.addUsers()
+        retrCommand = new RETRCommand( sql )
+        
+    }     // run before the first feature method
+    
+    def cleanupSpec() {
+        sql.execute "DELETE FROM email_user where username in ( ${gwRETR}, ${jaRETR}, ${joRETR} )"
+        sql.close()
+    }   // run after the last feature method
+   
+    def addUsers() {
+        def params = [ gwRETR, ( new Sha512Hash( somePassword, gwRETR, iterations ).toBase64() ), 'SHA-512', iterations, 'George', 'Washington', 0 ]
+        sql.execute 'insert into  email_user( username, password_hash, password_algo, iterations, first_name, last_name, version ) values ( ?, ?, ?, ?, ?, ?, ? )', params
+        
+        params = [ jaRETR, ( new Sha512Hash( somePassword, jaRETR, iterations ).toBase64() ), 'SHA-512', iterations, 'John', 'Adams', 0 ]
+        sql.execute 'insert into  email_user( username, password_hash, password_algo, iterations, first_name, last_name, version ) values ( ?, ?, ?, ?, ?, ?, ? )', params
+        
+        params = [ joRETR, ( new Sha512Hash( somePassword, joRETR, iterations ).toBase64() ), 'SHA-512', iterations, 'Jack', "O'Neill", 0 ]
+        sql.execute 'insert into  email_user( username, password_hash, password_algo, iterations, first_name, last_name, version ) values ( ?, ?, ?, ?, ?, ?, ? )', params
+        // sql.commit()
+        this.addMessage( uuidA, gwRETR, msgA )
+        this.addMessage( uuidB, gwRETR, msgB )
+        this.addMessage( uuidC, gwRETR, msgC )
+        theTimestamp = Timestamp.create()
+    }
+    
+    def addMessage( uuid, userName, messageString ) {
+        def toAddress = "${userName}@${domainList[ 0 ]}".toString()
+        def params = [ uuid, userName, 'hello@test.com', toAddress, messageString ]
+        sql.execute 'insert into mail_store(id, username, from_address, to_address, text_body) values (?, ?, ?, ?, ?)', params
+    }
+
+    def "test uuid list"() {
+        def uuidList = []
+        def uuid = UUID.randomUUID()
+        def totalMessageSizeTest = msgA.size() + msgB.size() + msgC.size()
+        def bufferInputMap = [:]
+        def timestamp
+        bufferInputMap.state = 'TRANSACTION'
+        bufferInputMap.timestamp = theTimestamp
+        def userInfo = [:]
+	    userInfo.username = gwRETR
+        bufferInputMap.userInfo = userInfo
+        sleep( 2 * 1000 )
+        when:
+            def resultMap = retrCommand.process( 'RETR', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "-ERR Command not in proper form"
+       /*
+            "+OK ${totalMessageSizeTest}\r\n" +
+            "1 ${msgA.size()}\r\n" +
+            "2 ${msgB.size()}\r\n" +
+            "3 ${msgC.size()}"
+        */
+        def messageStringB = 'aw' * 11
+        def toAddress = "${gwRETR}@${domainList[ 0 ]}".toString()
+        when:
+            bufferInputMap = resultMap.bufferMap
+            def params = [ UUID.randomUUID(), gwRETR, 'hello@test.com', toAddress, messageStringB ]
+            sql.execute 'insert into mail_store(id, username, from_address, to_address, text_body) values (?, ?, ?, ?, ?)', params    
+            def messageCount
+        
+            sql.eachRow( 'select count(*) from mail_store where username = ?', [ gwRETR ] ) { nextRow ->
+                messageCount = nextRow.count
+            }
+        then:
+            messageCount == 4
+        when:
+            resultMap = retrCommand.process( 'RETR', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "-ERR Command not in proper form"
+            /*
+            "+OK ${totalMessageSizeTest}\r\n" +
+            "1 ${msgA.size()}\r\n" +
+            "2 ${msgB.size()}\r\n" +
+            "3 ${msgC.size()}"
+            */
+	}
+
+	
+	def "test individual messages"() {
+        def uuidList = []
+        def uuid = UUID.randomUUID()
+        def totalMessageSizeTest = msgA.size() + msgB.size() + msgC.size()
+        def bufferInputMap = [:]
+        def timestamp
+        bufferInputMap.state = 'TRANSACTION'
+        bufferInputMap.timestamp = theTimestamp
+        def userInfo = [:]
+	    userInfo.username = gwRETR
+        bufferInputMap.userInfo = userInfo
+        sleep( 2 * 1000 )
+        when:
+            def resultMap = retrCommand.process( 'RETR 1', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "+OK ${msgA.size()} octets${crlf}" + "${msgA}${crlf}" + "." // +OK 1 20(\r\naqaqaqaqaqaqaqaqaqaq\r\n.)
+        
+        when:
+            resultMap = retrCommand.process( 'RETR 3', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "+OK ${msgC.size()} octets${crlf}" + "${msgC}${crlf}" + "." 
+            // resultMap.resultString == "+OK 3 ${msgC.size()}"
+            
+        when:
+            resultMap = retrCommand.process( 'RETR 2', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "+OK ${msgB.size()} octets${crlf}" + "${msgB}${crlf}" + "." 
+            // resultMap.resultString == "+OK 2 ${msgB.size()}"
+        
+        def messageStringB = 'aw' * 11
+        def toAddress = "${gwRETR}@${domainList[ 0 ]}".toString()
+        when:
+            bufferInputMap = resultMap.bufferMap
+            def params = [ UUID.randomUUID(), gwRETR, 'hello@test.com', toAddress, messageStringB ]
+            sql.execute 'insert into mail_store(id, username, from_address, to_address, text_body) values (?, ?, ?, ?, ?)', params    
+            def messageCount
+        
+            sql.eachRow( 'select count(*) from mail_store where username = ?', [ gwRETR ] ) { nextRow ->
+                messageCount = nextRow.count
+            }
+        then:
+            messageCount == 5
+        
+        when:
+            resultMap = retrCommand.process( 'RETR 4', [] as Set, bufferInputMap )
+        then:
+            resultMap.bufferMap.totalMessageSize == totalMessageSizeTest
+            resultMap.resultString == "-ERR no such message, only 3 messages in maildrop"
+	}
+
+}
+
